b34404ee1fe1597b0adb0b3add477a1e82260d37
henning mueller <henning@orgizm.net>
2014-10-12 20:01:08 +0200

    Validations mostly from PowerDNS on Rails.

--- lib/powerdns_db_cli/domain.rb
+++ lib/powerdns_db_cli/domain.rb
@@ -1,6 +1,36 @@
 module PowerDNS
   module DB
     class Domain < ActiveRecord::Base
+      IP_ADDR_REGEX = /\A(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\z/
+      TYPES = %w[NATIVE MASTER SLAVE]
+
+      has_many :records, dependent: :destroy
+
+      self.inheritance_column = :sti
+
+      validates :name,
+        presence: true,
+        uniqueness: true
+
+      validates :type,
+        inclusion: {
+          in: TYPES,
+          message: "has to be one of #{TYPES.join(', ')}"
+        }
+
+      validates :master,
+        if: :slave?,
+        presence: true,
+        format: {
+          with: IP_ADDR_REGEX,
+          message: 'has to be IP address'
+        }
+
+      default_scope { order :name }
+
+      def slave?
+        self.type == 'SLAVE'
+      end
     end
   end
 end
--- lib/powerdns_db_cli/record.rb
+++ lib/powerdns_db_cli/record.rb
@@ -1,6 +1,40 @@
 module PowerDNS
   module DB
     class Record < ActiveRecord::Base
+      belongs_to :domain
+
+      self.inheritance_column = :sti
+
+      validates :domain_id,
+        presence: true
+
+      validates :name,
+        presence: true
+
+      validates :ttl,
+        numericality: {
+          only_integer: true,
+          greater_than_or_equal_to: 0
+        }
+
+      before_validation :append_domain_name!, if: :domain_id?
+      before_save :update_change_date
+
+      private
+
+      def append_domain_name!
+        if self.name.blank?
+          self.name = self.domain.name
+        else
+          unless self.name.include?(self.domain.name)
+            self.name << ".#{self.domain.name}" 
+          end
+        end
+      end
+
+      def update_change_date
+        self.change_date = Time.now.to_i
+      end
     end
   end
 end
